bdd8eeb5b70e885292de52bcd24b265bff3ed4d9
Fitri <ftrannisaa@gmail..com>
2020-12-02 12:18:40 +0700

    setup login using jwt

---
+++ app/Pengguna.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+// use App\Scope\SoftDeleteScope;
+
+/**
+ * @property string $id_user
+ * @property string $email
+ * @property string $password
+ * @property string $nama
+ * @property string $create_data
+ * @property string $last_update
+ * @property integer $soft_delete
+ * @property int $id_peran
+ * @property string $id_pegawai
+ */
+class Pengguna extends Authenticatable implements JWTSubject
+{
+    
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    // protected static function boot()
+    // {
+    //     parent::boot();
+
+    //     static::addGlobalScope(new SoftDeleteScope);
+    // }
+
+    const CREATED_AT = 'create_date';
+    const UPDATED_AT = 'last_update';
+
+    protected $dateFormat = 'U';
+    public $timestamps = false;
+
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'pengguna';
+
+    /**
+     * The primary key for the model.
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'id_user';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * 
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['id_user', 'email', 'password', 'nama', 'create_data', 'last_update', 'soft_delete', 'id_peran', 'id_pegawai'];
+
+}
--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ],
 
     /*
@@ -208,6 +209,8 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Http' => Illuminate\Support\Facades\Http::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class, 
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
