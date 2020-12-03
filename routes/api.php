2fa02886e68e7855d35f40b3e44c6ec32d671831
ahmad apandi <ahmadapandi68@gmail.com>
2020-12-03 13:19:13 +0700

    push cors

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('login', 'LoginController@_login');
+Route::post('login', 'LoginController@_login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
